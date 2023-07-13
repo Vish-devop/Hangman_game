5656a230717e2ce3dc45d05cef36230e7320ff22
Sharma <Vishal.Sharma@insight.com>
2023-07-13 13:09:28 +0530

    added hangman_words file

--- Picking_a_random_words.py
+++ Picking_a_random_words.py
@@ -1,16 +1,102 @@
 # from collections import random
 import random
 
-#creating a list of words: 
-words=['ardvark','baboon','camel']
+#Taking out the words from hangman.py 
+from hangman_words import words
+
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 chossen_word=random.choice(words)
 print(chossen_word)
 
-guess=input("Guess the letter ")
-guess.lower()
 
-if guess in chossen_word:
-    print(True)
-else: 
-    print(False)
+display=[]
+for i in range(0,len(chossen_word)):
+    display.append("_")
+
+end_of_game=False
+lives=6
+while not end_of_game:
+
+    #ckecking guessed letter
+    guess=input("Guess the letter ").lower()
+    for position in range(0,len(chossen_word)):
+        letter=chossen_word[position]
+        if letter==guess:
+            display[position]=letter
+    
+    print(display)
+
+    if guess not in chossen_word:
+        lives-=1
+    if lives==0:
+        end_of_game=True
+        print("You lost")
+    
+    print(f"{' '.join(display)}")
+    
+    #checking if all the letters entered by the user are present in the choosen_word.
+    if "_" not in display:
+        end_of_game=True
+        print("You won")
+    
+    print(stages[lives])
+
+
+
